0adf4950cf3f5e5878dc37f5528cd83a4a0a1400
上原豪 <ueharago@ueharatsuyoshinoMacBook-puro.local>
2021-09-21 14:38:22 +0900

    set code for deploing to Heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const axios = require('axios');
+const port = process.env.PORT || 3000
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -39,6 +40,6 @@ app.get("/results", async (req, res) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log("Server started at port 3000")
+app.listen(port, () => {
+    console.log("Server started")
 })
